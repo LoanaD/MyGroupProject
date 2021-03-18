ec9a5132be09db3973f10c27c976dd23ee320965
Loana M. Davis <davisl41@prclab1.erau.edu>
2021-03-18 14:11:17 -0700

    first time project

---
+++ project.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+This is a program to play the game WAR.
+It's a 1 vs 1 for computer and a player.
+This program will promt you to enter random numbers from 0 to 25.
+
+
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+
+//generate 52 random ordered number within the range from 1 to 13(4 times)
+//devide it into 2 arrays. one is for computer and another for the player.
+//promt the player to enter the index of the the array from 0 to 25
+//print the number that in that index   = print array[input]
+//generate a random number within the range from 0 to 25
+
+int main()
+{
+    printf("Let's play the War card game!");
+
+    return 0;
+}
+
